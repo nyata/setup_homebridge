e4614e11f271aca751e7c735155f8ddbc146cdd1
nagisa yata <kanbai.8ta.n@gmail.com>
2016-12-23 14:14:17 +0900

    Change for sudo npm

--- setup.sh
+++ setup.sh
@@ -11,11 +11,17 @@ sudo apt-get purge -y nodejs npm
 curl -L git.io/nodebrew | perl - setup
 echo "export PATH=$HOME/.nodebrew/current/bin:$PATH" >> ~/.bashrc
 source ~/.bashrc
-sudo nodebrew install-binary stable
-sudo nodebrew use stable
+nodebrew install-binary stable
+nodebrew use stable
+
+# for sudo npm
+sudo ln -s /home/pi/.nodebrew/current/bin/node /usr/bin/node
+sudo ln -s /home/pi/.nodebrew/current/lib/node /usr/lib/node
+sudo ln -s /home/pi/.nodebrew/current/bin/npm /usr/bin/npm
+sudo ln -s /home/pi/.nodebrew/current/lib/npm /usr/lib/npm
 
 # Install Avahi
-apt-get install libavahi-compat-libdnssd-dev
+sudo apt-get install libavahi-compat-libdnssd-dev
 
 # Node modules global path
 NODE_PATH=`npm -g root`
@@ -24,7 +30,7 @@ NODE_PATH=`npm -g root`
 sudo npm install -g --unsafe-perm homebridge-config-server
 sudo npm install -g homebridge-multi
 cd $NODE_PATH/homebridge-config-server/
-npm install
+sudo npm install
 
 # Create config.json
 cd .homebridge
